6666de698b333f800536d656ccb478f4754cb947
Sam Wong <swong692@uwo.ca>
2020-09-22 11:14:21 -0400

    changed scientific notation

--- assign2.py
+++ assign2.py
@@ -2,45 +2,50 @@ import string
 import numpy as np
 import matplotlib.pyplot as plt
 
-def physconst(input):
+def physconst(const):
   """Takes input as the name of a physical constant and outputs its numerical value"""
 
-  input = input.lower
+  const = const.lower()
 
-  if(input == "speed of light"):
-    output = 2.99792458e8
-  elif (input == "planck's constant"):
-    output = 6.62606896e−34	
-  elif (input == "boltzmann's constant"):
-    output = 1.3806504e−23	
-  elif (input == "electron mass"):
-    output = 9.10938215e−31
-  elif (input == "atomic mass unit"):
-    output = 1.660538782e−27	
-  elif (input == "electron charge"):
-    output == 1.602176487e−19
-  elif (input == "bohr radius"):
-    output = 5.2917720859e−11
-  elif (input == "gravitational constant"):
-    output = 6.67428e−11
-  elif (input == "stefan-boltzmann constant"):
-    output = 5.670400e−8
+  if(const == "speed of light"):
+    output = 2.99792458 * 10**(8)
+  elif (const == "planck's constant"):
+    output = 6.62606896 * 10**(-4)
+  elif (const == "boltzmann's constant"):
+    output = 1.3806504 * 10**(-23)
+  elif (const == "electron mass"):
+    output = 9.10938215 * 10**(-31)
+  elif (const == "atomic mass unit"):
+    output = 1.660538782 * 10**(-27)
+  elif (const == "electron charge"):
+    output == 1.602176487 * 10**(-19)
+  elif (const == "bohr radius"):
+    output = 5.2917720859 * 10**(-11)
+  elif (const == "gravitational constant"):
+    output = 6.67428 * 10**(-11)
+  elif (const == "stefan-boltzmann constant"):
+    output = 5.670400 * 10**(-8)
   else:
     print("The value entered was invalid")
   return output
 
 
-def planck(np.temperatures, np.frequencies):
+def planck(temperatures, frequencies):
   """Returns the physical constants that compute the Planck radiation function""" 
   h=physconst("planck's constant")
   c=physconst("speed of light")
   k=physconst("boltzmann's constant")
 
-  np.boltzfn = (2*h*(np.frequencies**3)*(c**2))*((exp(h*np.frequencies)/k*np.temperatures)-1)**(-1))
+  boltzfn = (2*h*(np.frequencies**3)*(c**2))*((exp(h*np.frequencies)/k*np.temperatures)-1)**(-1)
 
-  print np.boltzfn
+  print(boltzfn)
 
   return()
 
 
-np.temps = [5000,10000,15000]
+temps = np.array([5000,10000,15000])
+lambdas = np.arrange(10, 2000)
+
+print(temps)
+print(lambdas)
+
